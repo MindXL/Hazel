e0d12b187968dc68f9869d2e08b38fea502c1895
Mind <MindXL@users.noreply.github.com>
2022-12-28 14:06:26 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+}
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,10 @@
+#include <Hazel.h>
+
+class Sandbox :public Hazel::Application
+{
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
