889a14f3e60d5b8df2c0504cdc58f348e0977c6e
Mind <MindXL@users.noreply.github.com>
2022-12-29 22:42:40 +0800

    Removed not explicitly used includes.

--- Hazel/src/pch.h
+++ Hazel/src/pch.h
@@ -2,18 +2,9 @@
 
 #include <iostream>
 #include <memory>
-#include <utility>
-#include <algorithm>
 #include <functional>
 
 #include <string>
 #include <sstream>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-
-#ifdef HZ_PLATFORM_WINDOWS
-#include <Windows.h>
-#endif // HZ_PLATFORM_WINDOWS
 
 #include "Hazel/Log.h"
