f62025d8569ade8ad8d061694b4973cadf0d9c07
Mind <MindXL@users.noreply.github.com>
2022-12-30 02:45:25 +0800

    Added ImGui and basic ImGui layer

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -20,6 +20,10 @@ namespace Hazel {
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 
+		inline Window& GetWindow() { return *m_Window; }
+
+		inline static Application& Get() { return *s_Instance; }
+
 	private:
 		bool OnWindowClose(WindowCloseEvent& event);
 
@@ -27,6 +31,8 @@ namespace Hazel {
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
---
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+
+#include "ImGuiLayer.h"
+#include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+#include "Hazel/Application.h"
+
+#include <GLFW/glfw3.h>
+
+namespace Hazel {
+	ImGuiLayer::ImGuiLayer()
+		:Layer{ "ImGuiLayer" } {}
+
+	ImGuiLayer::~ImGuiLayer() {}
+
+	void ImGuiLayer::OnAttach()
+	{
+		ImGui::CreateContext();
+		ImGui::StyleColorsDark();
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
+		io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+
+		ImGui_ImplOpenGL3_Init("#version 460");
+	}
+
+	void ImGuiLayer::OnDetach() {}
+
+	void ImGuiLayer::OnUpdate()
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		Application& app = Application::Get();
+
+		io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
+
+		float time = (float)glfwGetTime();
+		io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f / 60.0f);
+		m_Time = time;
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui::NewFrame();
+
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+
+	void ImGuiLayer::OnEvent(Event& event) {}
+}
