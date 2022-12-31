9d906d11c39d45ad4060aa78be33231605801205
Mind <MindXL@users.noreply.github.com>
2023-01-01 04:21:16 +0800

    feat: Added docking and viewports from ImGui
    Switched ImGui to docking branch
    
    Using ImGui's GLFW/OpenGL implementation

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -16,23 +16,9 @@ namespace Hazel
 
 		void OnAttach() override;
 		void OnDetach() override;
-		void OnUpdate() override;
-		void OnEvent(Event& event) override;
 
-	private:
-		// Events
-		bool OnWindowResizeEvent(WindowResizeEvent& event);
-
-		bool OnMouseMovedEvent(MouseMovedEvent& event);
-		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& event);
-		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event);
-		bool OnMouseScrolledEvent(MouseScrolledEvent& event);
-
-		bool OnKeyPressedEvent(KeyPressedEvent& event);
-		bool OnKeyReleasedEvent(KeyReleasedEvent& event);
-		bool OnKeyTypedEvent(KeyTypedEvent& event);
-
-	private:
-		float m_Time = 0.0f;
+		void Begin();
+		void OnImGuiRender() override;
+		void End();
 	};
 }
--- Hazel/src/Hazel/LayerStack.h
+++ Hazel/src/Hazel/LayerStack.h
@@ -28,6 +28,6 @@ namespace Hazel
 
 	private:
 		std::vector<Layer*> m_Layers;
-		std::vector<Layer*>::iterator m_LayerInsert;
+		unsigned int m_LayerInsertIndex = 0;
 	};
 }
