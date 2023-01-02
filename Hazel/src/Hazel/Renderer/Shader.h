14c45b276ba8d09a4c81f828e3242723159b0524
Mind <MindXL@users.noreply.github.com>
2023-01-02 17:20:40 +0800

    feat: Added shader

---
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace Hazel
+{
+	class Shader
+	{
+	public:
+		/*
+		* @param vertexSource: source code for vertex shader.
+		* @param fragmentSource: source code for fragment shader.
+		*/
+		Shader(const std::string& vertexSource, const std::string& fragmentSource);
+		~Shader();
+
+		void Bind() const;
+		void Unbind() const;
+
+	private:
+		uint32_t m_RendererID;
+	};
+}
