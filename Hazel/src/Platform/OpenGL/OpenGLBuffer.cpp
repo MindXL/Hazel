2174688a601e315bdf549e45458a53c02ad112c0
MindXL <MindXL@users.noreply.github.com>
2023-01-23 12:58:04 +0800

    fix: Changed bind target inside OpenGLIndexBuffer constructor

--- Hazel/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -34,8 +34,11 @@ namespace Hazel
 		: m_Count{count}
 	{
 		glCreateBuffers(1, &m_RendererID);
-		Bind();
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, (long long)(count * sizeof(float)), indices, GL_STATIC_DRAW);
+
+		// GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO.
+		// Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state. 
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, (long long)(count * sizeof(float)), indices, GL_STATIC_DRAW);
 	}
 
 	OpenGLIndexBuffer::~OpenGLIndexBuffer()
