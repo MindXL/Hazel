b7622c5a2baf16ec5c85d0fa25bc044b27ad5acc
MindXL <MindXL@users.noreply.github.com>
2023-01-23 23:13:42 +0800

    feat: Added check for OpenGL version

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,6 +24,15 @@ namespace Hazel
 		HZ_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
 		HZ_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
 		HZ_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
+
+#ifdef HZ_ENABLE_ASSERTS
+		int versionMajor, versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		HZ_CORE_ASSERT((versionMajor == 4 && versionMinor >= 6)|| versionMajor > 4,
+		               "Hazel requires at least OpenGL version 4.6!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
