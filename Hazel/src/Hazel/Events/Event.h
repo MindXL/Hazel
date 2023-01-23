26ea9547bd2badd9b51c23fa5e44cdf638d6c7f2
MindXL <MindXL@users.noreply.github.com>
2023-01-23 13:14:42 +0800

    maintain: Avoid using std::function in event dispatcher

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -52,19 +52,16 @@ namespace Hazel
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
-
 	public:
 		explicit EventDispatcher(Event& event)
 			: m_Event{event} {}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		template<typename T, typename Func>
+		bool Dispatch(const Func& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
