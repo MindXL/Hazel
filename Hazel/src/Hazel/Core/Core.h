39adc49e0844622a7980fc211d367db7d1cdde5c
MindXL <MindXL@users.noreply.github.com>
2023-01-23 22:35:21 +0800

    feat: Added automated platform detection using predefined macro's

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -2,31 +2,71 @@
 
 #include <memory>
 
-// TODO: Fix or stop using VS auto formatting on save.
+// Platform detection using predefined macros
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#ifdef _WIN64
+		/* Windows x64  */
+		#define HZ_PLATFORM_WINDOWS
+	#else
+		/* Windows x86 */
+		#error "x86 Builds are not supported!"
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define HZ_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define HZ_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#define HZ_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define HZ_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#else
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif // End of platform detection
+
 #ifdef HZ_PLATFORM_WINDOWS
-#ifdef HZ_DYNAMIC_LINK
-#ifdef HZ_BUILD_DLL
-	#define HAZEL_API __declspec(dllexport)
-#else
-	#define HAZEL_API __declspec(dllimport)
-#endif // HZ_BUILD_DLL
-#else
-#define HAZEL_API
-#endif // HZ_DYNAMIC_LINK
+	#ifdef HZ_DYNAMIC_LINK
+		#ifdef HZ_BUILD_DLL
+			#define HAZEL_API __declspec(dllexport)
+		#else
+			#define HAZEL_API __declspec(dllimport)
+		#endif // HZ_BUILD_DLL
+	#else
+		#define HAZEL_API
+	#endif // HZ_DYNAMIC_LINK
 #else
 	#error Hazel only supports Windows!
 #endif // HZ_PLATFORM_WINDOWS
 
 #ifdef HZ_DEBUG
-#define HZ_ENABLE_ASSERTS
+	#define HZ_ENABLE_ASSERTS
 #endif
 
 #ifdef HZ_ENABLE_ASSERTS
-#define HZ_CORE_ASSERT(x, ...) [&]() { if (!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }()
-#define HZ_CLIENT_ASSERT(x, ...) [&]() { if (!(x)) { HZ_CLIENT_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }()
+	#define HZ_CORE_ASSERT(x, ...) [&]() { if (!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }()
+	#define HZ_CLIENT_ASSERT(x, ...) [&]() { if (!(x)) { HZ_CLIENT_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }()
 #else
-#define HZ_CORE_ASSERT(x, ...)
-#define HZ_CLIENT_ASSERT(x, ...)
+	#define HZ_CORE_ASSERT(x, ...)
+	#define HZ_CLIENT_ASSERT(x, ...)
 #endif // HZ_ENABLE_ASSERTS
 
 static consteval int BIT(const int x) { return 1 << x; }
